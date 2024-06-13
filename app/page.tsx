fe2f6b810ec9b740bfb28488dacd62a705115bd3
wanghaisheng <edwin.uestc@gmail.com>
2024-06-13 13:14:45 +0800

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      Hello subfolder.{" "}
+      <Link href="/about">about.tsx subfolder under /app/about</Link>
+      Hello samefolder.
+      <Link href="/samefolder">samefolder.tsx under /app</Link>
     </div>
-  )
+  );
 }
--- app/samefolder.tsx
+++ app/samefolder.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function About() {
+export default function Samefolder() {
   return (
     <>
-      <h1>sub folder in app</h1>
+      <h1>samefolder in app</h1>
       <div>
         Back to <Link href="/">Home</Link>
       </div>
